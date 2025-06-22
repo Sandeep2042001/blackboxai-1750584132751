4edc8c9c80c43950dd7b04b3a96f509ba5cba025
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2025-06-22 14:52:42 +0530

    Add src/main/java/com/henuka/imitations/HenukaImitationsApplication.java 1750584160667

---
+++ src/main/java/com/henuka/imitations/HenukaImitationsApplication.java
@@ -0,0 +1,11 @@
+package com.henuka.imitations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenukaImitationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HenukaImitationsApplication.class, args);
+    }
+}
